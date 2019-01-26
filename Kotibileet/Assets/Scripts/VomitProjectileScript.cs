05eefef2c5aa173bf0bf9d3e123e184177636261
VSP <henri.vesterinen@gmail.com>
2019-01-26 12:34:14 +0200

    Vomiting
    Basic Vomiting Script and Vomit Particle Script.

---
+++ Kotibileet/Assets/Scripts/VomitProjectileScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VomitProjectileScript : MonoBehaviour
+{
+    public Transform stainPrefab;
+
+    public float lifeTime = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnDestroy()
+    {
+        Transform stain = Instantiate(stainPrefab, gameObject.transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Kotibileet/Assets/Scripts/VomitScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class VomitScript : MonoBehaviour
+{
+    public Transform vomitPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            puke();
+        }
+    }
+
+    public void puke()
+    {
+        Transform vomitParticle = Instantiate(vomitPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        vomitParticle.GetComponent<Rigidbody2D>().velocity = new Vector3(15, Random.Range(-3, 3), 0);
+        Debug.Log("Space pressed");
+    }
+
+    public GameObject PullOBJ;
+    public float ForceSpeed;
+
+    public void OnTriggerStay(Collider coll)
+    {
+
+        Debug.Log("Vomit detected!");
+
+        if (coll.gameObject.tag == ("Vacuumable"))
+        {
+            PullOBJ = coll.gameObject;
+
+            PullOBJ.transform.position = Vector3.MoveTowards
+                (PullOBJ.transform.position,
+                 transform.position,
+                 ForceSpeed * Time.deltaTime);
+        }
+
+    }
+}
