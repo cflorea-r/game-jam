c95c8431aab03942c39cce2c297ac3b359dc6376
flciprian <46439274+flciprian@users.noreply.github.com>
2019-01-26 15:46:53 +0200

    Booze spawn and pickup

---
+++ Kotibileet/Assets/Scripts/BoozeSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoozeSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 1, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Spawn()
+    {
+        int randomIndex = Random.Range(0, transform.childCount - 1);
+        transform.GetChild(randomIndex).gameObject.SetActive(true);
+    }
+}
