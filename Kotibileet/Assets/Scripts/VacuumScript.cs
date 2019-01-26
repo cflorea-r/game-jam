4ef2af446c4fbecb3d7414f4e62c044fbcc9064f
VSP <henri.vesterinen@gmail.com>
2019-01-26 19:12:07 +0200

    vacuum
    Destroy puke on collision

--- Kotibileet/Assets/Scripts/VacuumScript.cs
+++ Kotibileet/Assets/Scripts/VacuumScript.cs
@@ -18,12 +18,25 @@ public class VacuumScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(vacuuming == true)
+        if (vacuuming == true)
         {
-            if(Vector3.Distance(attractedTo.transform.position, transform.position) <= 3)
+            if (Vector3.Distance(attractedTo.transform.position, transform.position) <= 3)
             {
                 transform.position = Vector3.MoveTowards(transform.position, attractedTo.transform.position, step);
             }
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if(other.GetComponent<PlayerControl>() == true)
+        {
+            if (other.GetComponent<PlayerControl>().playerNumber == 1)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+
     }
 }
