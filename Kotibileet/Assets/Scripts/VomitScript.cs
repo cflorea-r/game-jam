489503bb7c39435e0fba69c81cc675fe9b50462f
VSP <henri.vesterinen@gmail.com>
2019-01-26 13:01:09 +0200

    more vomiting
    Vomit projectiles now have boxcolliders and can push players.

--- Kotibileet/Assets/Scripts/VomitScript.cs
+++ Kotibileet/Assets/Scripts/VomitScript.cs
@@ -22,32 +22,35 @@ public class VomitScript : MonoBehaviour
         {
             puke();
         }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        { 
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0, 0.1f, 0);
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0, -0.1f, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(-0.1f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0.1f, 0, 0);
+        }
+
     }
 
     public void puke()
     {
-        Transform vomitParticle = Instantiate(vomitPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        Transform vomitParticle = Instantiate(vomitPrefab, gameObject.transform.position, Quaternion.identity);
         vomitParticle.GetComponent<Rigidbody2D>().velocity = new Vector3(15, Random.Range(-3, 3), 0);
         Debug.Log("Space pressed");
     }
 
-    public GameObject PullOBJ;
-    public float ForceSpeed;
-
-    public void OnTriggerStay(Collider coll)
-    {
-
-        Debug.Log("Vomit detected!");
-
-        if (coll.gameObject.tag == ("Vacuumable"))
-        {
-            PullOBJ = coll.gameObject;
-
-            PullOBJ.transform.position = Vector3.MoveTowards
-                (PullOBJ.transform.position,
-                 transform.position,
-                 ForceSpeed * Time.deltaTime);
-        }
-
-    }
 }
