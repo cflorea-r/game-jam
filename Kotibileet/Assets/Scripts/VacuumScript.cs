e5215be248527886e29c962dc66667b382fec949
VSP <henri.vesterinen@gmail.com>
2019-01-26 16:30:26 +0200

    Vacuuming script barebones
    currently vacuums all gameobjects with Vacuum script without restrictions.

---
+++ Kotibileet/Assets/Scripts/VacuumScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumScript : MonoBehaviour
+{
+    public GameObject attractedTo;
+    float step = 0.2f;
+    public static bool vacuuming;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(vacuuming == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, attractedTo.transform.position, step);
+        }
+    }
+}
